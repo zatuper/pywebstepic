8e1051e77294c9cc99020a98c345806531b1c089
Billi <ztpp99@mail.ru>
2016-03-31 17:59:42 +0300

    s.py простой TCP echo server
    Signed-off-by: Billi <ztpp99@mail.ru>
    
     Author:    Billi <ztpp99@mail.ru>
     Date:      Thu Mar 31 17:59:42 2016 +0300
    
    Signed-off-by: Billi <ztpp99@mail.ru>

---
+++ s.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(10)
+while True:
+    conn, addr = s.accept()
+    while True:
+                data = conn.recv(1024)
+          if not data: break
+          conn.send(data)
+          if data == "close" or data == "Close":break
+    conn.close()
