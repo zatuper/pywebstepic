a974cc8e7a45146ce922b874609df49076ac1c0d
Billi <leichtelecturen@gmail.com>
2016-04-07 11:57:17 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 #sudo rm -rf /etc/nginx/sites-enabled/default
+sudo mv /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default2                                          
+                                                                                                  
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
