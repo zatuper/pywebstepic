a553dfe4efe95c77a823c9f176dde53da8e331d8
s@s.r <s@s.r>
2016-04-07 08:43:57 +0000

    init.sh опечатка

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
