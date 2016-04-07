7971f08cf19e9bc4a378b92f4d5591b706474f2c
Billi <leichtelecturen@gmail.com>
2016-04-07 10:39:25 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,8 @@ cp id_rsa ~/.ssh/id_rsa
 cp id_rsa.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
-eval "$(ssh-agent -s)"
-eval "$(ssh-agent)"   
+# eval "$(ssh-agent -s)"
+# eval "$(ssh-agent)" 
+exec ssh-agent bash
 ssh-add ~/.ssh/id_rsa
 #loook for more complex way to do the same at http://pastebin.com/88rpiTiF
