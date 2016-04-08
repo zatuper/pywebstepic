f82803a04132d86402dd284572c7309d66e37885
Billi <leichtelecturen@gmail.com>
2016-04-08 14:09:50 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -12,7 +12,10 @@ def number_of_workers():
 
 
 def handler_app(environ, start_response):
-    response_body = b'Hello world!'
+    resp = []
+    response_body = environ['QUERY_STRING'].split("&")
+    for arg in response_body:
+        resp = [arg+"\r\n"]
     status = '200 OK'
 
     response_headers = [
@@ -21,7 +24,16 @@ def handler_app(environ, start_response):
 
     start_response(status, response_headers)
 
-    return [response_body]
+    return [resp]
+
+status = '200 OK'
+  response_headers = [('Content-type','text/plain')]
+  start_response(status, response_headers)
+  resp = environ['QUERY_STRING'].split("&")
+  resp = [item+"\r\n" for item in resp]
+  return resp
+
+
 
 
 class StandaloneApplication(gunicorn.app.base.BaseApplication):
@@ -42,7 +54,7 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
 
 if __name__ == '__main__':
     options = {
-        'bind': '%s:%s' % ('127.0.0.1', '8080'),
+        'bind': '%s:%s' % ('0.0.0.0', '8080'),
         'workers': number_of_workers(),
     }
     StandaloneApplication(handler_app, options).run()
