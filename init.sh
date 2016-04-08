38e6ff14b6d69bfe97d656bf29ecc70ab89b2808
Billi <leichtelecturen@gmail.com>
2016-04-08 09:48:38 +0300

    try 1  to load ssh-agent
    exec - is stoping the script. according  https://stackoverflow.com/questions/25714239/how-to-execute-git-commands-from-bash-script

--- init.sh
+++ init.sh
@@ -15,12 +15,12 @@ cp id_rsa ~/.ssh/id_rsa
 cp id_rsa.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
-# eval "$(ssh-agent -s)"
-eval "$(ssh-agent)" 
-exec ssh-agent bash
+source eval "$(ssh-agent -s)"
+# eval "$(ssh-agent)" 
+# exec ssh-agent bash
 sleep 2
-ssh-add ~/.ssh/id_rsa
 #loook for more complex way to do the same at http://pastebin.com/88rpiTiF
 cd ~/web
 git remote set-url --push origin 'git@github.com:zatuper/pywebstepic.git'
+source ssh-add ~/.ssh/id_rsa
 ssh-add -l  
