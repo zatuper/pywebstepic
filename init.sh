16f1a268ec4912da6d199d6a1f1809637e8b2f8e
Billi <leichtelecturen@gmail.com>
2016-04-08 14:53:58 +0300

    remove gunicorn examples from /etc/

--- init.sh
+++ init.sh
@@ -2,9 +2,11 @@
 trap "set +x; sleep 1; set -x" DEBUG
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 #sudo rm -rf /etc/nginx/sites-enabled/default
-sudo mv /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default2                                          
+sudo mv /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default2   
+sudo mv /etc/gunicorn.d/django.example  /home/box/web/etc/django.example
+sudo mv /etc/gunicorn.d/wsgi.example  /home/box/web/etc/wsgi.example
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/etc/test /etc/gunicorn.d/test
 # sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
