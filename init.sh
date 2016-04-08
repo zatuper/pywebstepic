1b9a4f00d543ac85dfb287b07f1a7818b1373868
Billi <leichtelecturen@gmail.com>
2016-04-08 10:33:32 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,16 +9,7 @@ sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 
 alias ll='ls -ilhao --color=auto'
-mkdir ~/.ssh
-cp id_rsa ~/.ssh/id_rsa
-cp id_rsa.pub ~/.ssh/id_rsa.pub
-chmod 600 ~/.ssh/id_rsa.pub
-chmod 600 ~/.ssh/id_rsa
-source eval "$(ssh-agent -s)"
-# eval "$(ssh-agent)" 
-# exec ssh-agent bash
-sleep 2
-#loook for more complex way to do the same at http://pastebin.com/88rpiTiF
+
 cd ~/web
 git remote set-url --push origin 'git@github.com:zatuper/pywebstepic.git'
 source ssh-add ~/.ssh/id_rsa
