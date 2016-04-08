f1a1284dc9298c62f395c63a5a6da9112ccbc5c0
Billi <leichtelecturen@gmail.com>
2016-04-08 10:32:22 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,8 +7,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 # sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-git config user.email "s@s.r"
-git config user.name "s@s.r"
+
 alias ll='ls -ilhao --color=auto'
 mkdir ~/.ssh
 cp id_rsa ~/.ssh/id_rsa
