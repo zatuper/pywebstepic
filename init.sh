9f9bcd9475442e610c98d8e5791483ef1f2cacc0
Billi <leichtelecturen@gmail.com>
2016-04-08 16:57:46 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 trap "set +x; sleep 1; set -x" DEBUG
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -f /etc/nginx/nginx.conf
+sudo rm -f /etc/nginx/nginx.conf 
 sudo ln  /home/box/web/etc/nginx-main /etc/nginx/nginx.conf 
 
 #sudo rm -rf /etc/nginx/sites-enabled/default
-sudo mv /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default2   
-sudo mv /etc/gunicorn.d/django.example  /home/box/web/etc/django.example
-sudo mv /etc/gunicorn.d/wsgi.example  /home/box/web/etc/wsgi.example
+sudo rm -f /etc/nginx/sites-enabled/default   
+sudo rm -f /etc/gunicorn.d/django.example 
+sudo rm -f  /etc/gunicorn.d/wsgi.example  
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/test /etc/gunicorn.d/test
 # sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
