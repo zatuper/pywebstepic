33f42d46c9a09aa04c62045b3ac22da8206a6a2c
Billi <leichtelecturen@gmail.com>
2016-04-08 10:45:12 +0300

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -3,10 +3,14 @@ cp id_rsa ~/.ssh/id_rsa
 cp id_rsa.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
-source eval "$(ssh-agent -s)"
+#eval "$(ssh-agent -s)"
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+  exec ssh-agent bash -c "ssh-add ; $0"
+  exit
+fi
 # eval "$(ssh-agent)" 
 # exec ssh-agent bash
 sleep 2
 #loook for more complex way to do the same at http://pastebin.com/88rpiTiF
-source ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 ssh-add -l  
