0c1f44e7d1cff68a1db9e7ca1b6a1777eec5a061
Billi <leichtelecturen@gmail.com>
2016-04-08 10:33:56 +0300

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -1,1 +1,11 @@
 
+mkdir ~/.ssh
+cp id_rsa ~/.ssh/id_rsa
+cp id_rsa.pub ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa
+source eval "$(ssh-agent -s)"
+# eval "$(ssh-agent)" 
+# exec ssh-agent bash
+sleep 2
+#loook for more complex way to do the same at http://pastebin.com/88rpiTiF
