bc1f07b6796325ef4417b638a1124ccd8658ebcd
Billi <leichtelecturen@gmail.com>
2016-04-11 16:47:28 +0300

    py
    https://stackoverflow.com/questions/32525664/gunicorn-with-gevent-maintaining-per-request-global-data
    http://pythonfiddle.com/parseurlfordomaincomx1&y2&z4&x0

--- hello.py
+++ hello.py
@@ -11,14 +11,28 @@ CONFIG = {
     ),
 }
 
-spisok=['']
+def parse_url_string(url):
+    params = url.split('?')[1].split('&')
+    car = {}
+    for param in params:
+        car[param.split('=')[0]] = param.split('=')[1]
+    return (params, car)
+values, query = parse_url_string('domain.com/?x=1&y=2&z=4&x=0')
+
+#
+#print values,  query,  query.keys(), query.values() 
+#for key in query.keys():
+#    print key, "=", query[key],
+return (values,  query)     
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    resp = env['QUERY_STRING'].split("&")
-    print resp
-    for arg in resp:
-        spisok  = [arg+"\r\n"]
-        print spisok
-    
-    return [spisok]
+    url = env['QUERY_STRING']
+    print 'will send ', url
+    values, query = parse_url_string(url)
+    response=[]
+    for key in query.keys():
+        a = key + "=" + query[key] + "\n"
+        response+=a
+    print 'parsed url values', response         
+return [response, 'OK']
