e7d83c3884824b1a0e7cc5fddab2fa49741ac110
Billi <leichtelecturen@gmail.com>
2016-04-12 15:23:06 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -24,7 +24,7 @@ def application(env, start_response):
         if (param):
            print param
             # response[param.split('=')[0]] = param.split('=')[1]+"\n"
-           print url(param)     
+           print url[param]     
     print "now splited response ", response
 #    for key in response:
 #        if key:
