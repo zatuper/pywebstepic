e538c8744516bcd2a18052148bcd05b2df0a1bed
Billi <leichtelecturen@gmail.com>
2016-04-15 15:28:07 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -14,7 +14,7 @@ def application(env, start_response):
     url = []
     start_response('200 OK', [('Content-Type', 'text/plain')])
     url = (env['QUERY_STRING'])
-    url += "&z=0" #z=0 just for test when no url query
+    #url += "&z=0" #z=0 just for test when no url query
     url = url.split('&')
     print 'app:', url
     datastr=str(url)
@@ -24,4 +24,11 @@ def application(env, start_response):
     datastr='\n'.join(map(str, url)).replace("'", '').replace("[",'').replace("]",'')
     #url = '\r\n'.join([url]) 
     #url = '\r\n'.join([(env['QUERY_STRING'].split("&"))])
+    #print 'app: url', url
+-   #for e in url:
+-   #    if e:
+-   #       print 'app: element', e
+-   #       resp+='\n'+(str([e])).replace("'", '').replace("[",'').replace("]",'')
+-   #       #datastr='\n'.join(map(str, e)).replace("'", '').replace("[",'').replace("]",'')
+-   #print 'app - resp', resp 
     return datastr
