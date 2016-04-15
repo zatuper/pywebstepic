f73c74050e8202d66ce765a2e0ca73e24d7764ca
Billi <leichtelecturen@gmail.com>
2016-04-15 15:23:23 +0300

    simple the best

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    # 'mode': 'wsgi',
+    'mode': 'wsgi',
     'working_dir': '/path/to/my/app',
     'python': '/usr/bin/python',
     'args': (
@@ -10,21 +10,18 @@ CONFIG = {
     ),
 }
 
-
-  
-
 def application(env, start_response):
+    url = []
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    url = env['QUERY_STRING']
+    url = (env['QUERY_STRING'])
+    url += "&z=0" #z=0 just for test when no url query
     url = url.split('&')
-    resp=''
-    print 'app: url', url
-    for e in url:
-        if e:
-           print 'app: element', e
-           resp+='\n'+(str([e])).replace("'", '').replace("[",'').replace("]",'')
-           #datastr='\n'.join(map(str, e)).replace("'", '').replace("[",'').replace("]",'')
-    print 'app - resp', resp 
-    
-    return resp
-    
+    print 'app:', url
+    datastr=str(url)
+    print 'app:', datastr
+    datastr=' '.join(map(str, url))
+    print 'app:', datastr
+    datastr='\n'.join(map(str, url)).replace("'", '').replace("[",'').replace("]",'')
+    #url = '\r\n'.join([url]) 
+    #url = '\r\n'.join([(env['QUERY_STRING'].split("&"))])
+    return datastr
