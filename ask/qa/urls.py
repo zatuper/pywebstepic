5bcc0013fa5531bef6915c8bf1e3137ac2efd374
Billi <leichtelecturen@gmail.com>
2016-04-18 16:49:08 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -11,6 +11,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     # url(r'^$', 'handler404')
     url(r'^404/$', 'django.views.defaults.page_not_found')
-    url(r'^articles/(?P<ID_qa>\d{6})/$', 'qa.views.quest'),
+    url(r'^question/(?P<ID_qa>\d{6})/$', 'qa.views.quest'),
     
 )
