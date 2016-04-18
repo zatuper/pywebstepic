6b2b07bc0c05d4ddc188baa7821af4e8a73ffec6
Billi <leichtelecturen@gmail.com>
2016-04-18 17:25:59 +0300

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -6,7 +6,7 @@ USER=www-data                                        # the user to run as (*)
 GROUP=box                                     # the group to run as (*)
 NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn (*)
 DJANGO_SETTINGS_MODULE='/home/box/web/ask/ask/settings'             # which settings file should Django use (*)
-DJANGO_WSGI_MODULE='/home/box/web/ask/ask/ask.wsgi'                     # WSGI module name (*)
+DJANGO_WSGI_MODULE='/home/box/web/ask/ask/ask'                     # WSGI module name (*)
 
 echo "Starting $NAME as `whoami`"
 
@@ -23,8 +23,5 @@ export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
 # as no virtualnv no need of /var/www/test/venv/bin/
-exec gunicorn ${DJANGO_WSGI_MODULE}:application \ 
-  --name $NAME \
-  --workers $NUM_WORKERS \
-  --user $USER \
-  --bind='0.0.0.0:8000' #no socket unix:$SOCKFILE
+exec gunicorn ${DJANGO_WSGI_MODULE}:application --name $NAME --workers $NUM_WORKERS --user $USER --bind='0.0.0.0:8000'
+#no socket unix:$SOCKFILE
