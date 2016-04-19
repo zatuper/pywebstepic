93e4aca0d48ced4a80238aa9a6d9167f7a24924f
s@s.r <s@s.r>
2016-04-19 13:50:26 +0000

    todo: fix urls.py everywhere

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -2,15 +2,11 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+# from . import views
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    
-    url(r'^(?P<question>\d+)', include('qa.urls')),
-]
-    
-)
+    url(r'^$', 'ask.views.home', name='home'),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^(?P<question>\d+)', include('qa.urls'))
+    )
