26fb1a711382a0f0aa2e464a2412a5182da56472
Billi <leichtelecturen@gmail.com>
2016-04-19 17:19:47 +0300

    запятые

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,12 +5,12 @@
 #/ask/
 #/popular/
 #/new/
+# url(r'^$', 'handler404')
 
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    # url(r'^$', 'handler404')
-    url(r'^404/$', 'django.views.defaults.page_not_found')
+    url(r'^404/$', 'django.views.defaults.page_not_found'),
     url(r'^question/(?P<ID_qa>\d{6})/$', 'qa.views.quest'),
     
 )
