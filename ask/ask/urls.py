c389d4ba9fc32a4d298a0b9ca3c9604e37878f08
is@s.r <s@s.r>
2016-04-20 08:59:02 -0400

    ask login signup popular new handlers 	modified:   ask/ask/urls.py 	modified:   ask/ask/views.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,12 +3,18 @@ from django.conf.urls import patterns, include, url
 from . import views
 from django.contrib import admin
 admin.autodiscover()
-
+#/login/
+#/signup/
+#/question/<123>/    # вместо <123> - произвольный ID
+#/ask/
+#/popular/
+#/new/
 urlpatterns = [
     url(r'^login/', view.login, name='login'),
-    url(r'^signup/', view.login, name='signup'),
-    url(r'^ask/', view.login, name='ask'),
-    url(r'^popular/', view.login, name='popular'),    
+    url(r'^signup/', view.signup, name='signup'),
+    url(r'^ask/', view.ask, name='ask'),
+    url(r'^popular/', view.popular, name='popular'),
+    url(r'^new/', view.new, name='new'),
     url(r'^$',     include('qa.urls')),
     url(r'^question/([0-9]{4})/$', include('qa.urls')),
     url(r'^blog/', include('blog.urls')),
--- ask/ask/views.py
+++ ask/ask/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
-# ask
+#/login/
+#/signup/
+#/question/<123>/    # вместо <123> - произвольный ID
+#/ask/
+#/popular/
+#/new/
+
+
 def home(request):
     return HttpResponse('Hello World')
+def login(request):
+    return HttpResponse('')
+def signup(request):
+    return HttpResponse('')
+def ask(request):
+    return HttpResponse('')
+def popular(request):
+    return HttpResponse('')
+def new(request):
+    return HttpResponse('')
+  
