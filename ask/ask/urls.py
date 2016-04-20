d7738fcf88c355dfd3bc46c1d75dc94c322b8e61
is@s.r <s@s.r>
2016-04-20 06:42:33 -0400

    modified:   ask/ask/urls.py         url(r'^question/([0-9]{4})/$', include('qa.urls'))         cам qa.url пis empty

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -6,5 +6,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$',     include('qa.urls')),
+    url(r'^question/([0-9]{4})/$', include('qa.urls')),
     url(r'^blog/', include('blog.urls')),
+ 
 ]
