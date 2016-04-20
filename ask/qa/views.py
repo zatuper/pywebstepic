95d60a8207c7b4ffc79b882a6538c90826f75646
is@s.r <s@s.r>
2016-04-20 07:10:44 -0400

    modified:   ask/qa/views.py http request to view

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,12 +1,12 @@
 # ask/qa
 from django.shortcuts import render
+from django.utils.html import escape
 
 # Create your views here.
 from django.http import HttpResponse 
 
 def test (request, *args, **kwargs):
-    return HttpResponse('OK')
+    return HttpResponse(escape(repr(request)))
  
 def quest (request, *args, **kwargs):
     return HttpResponse('QUEST IS OK')
- 
