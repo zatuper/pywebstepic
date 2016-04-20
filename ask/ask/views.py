d670e72ca0727eb7e3d41353c12a7d165d213c25
Billi <leichtelecturen@gmail.com>
2016-04-20 16:17:44 +0300

    FIX: import HttpResponse

--- ask/ask/views.py
+++ ask/ask/views.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-
+from django.http import HttpResponse 
+# Create your views here.
 #/login/
 #/signup/
 #/question/<123>/    # вместо <123> - произвольный ID
 #/ask/
 #/popular/
 #/new/
-
 
 def home(request):
     return HttpResponse('Hello World')
