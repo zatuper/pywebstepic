339b79ce6714a8a29381a3d82168069ca1580d60
Billi <leichtelecturen@gmail.com>
2016-04-22 14:49:35 +0300

    Update venv.sh
    пока без virtualenv 
    TODO
    https://github.com/pypa/virtualenv/blob/develop/docs/installation.rst

--- venv.sh
+++ venv.sh
@@ -1,2 +1,4 @@
 python -c "import django;print (django.get_version())" #какая версия? 
 python -c "import sys; sys.path = sys.path[1:]; import django; print(django.__path__)" # где установлен?
+sudo pip install --upgrade django  
+
