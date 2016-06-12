59ffd969cb8dc0d602941d6676757cc4e9f7858b
Billi <ztpp99@mail.ru>
2016-06-12 12:36:55 +0000

    Signed-off-by: Billi <ztpp99@mail.ru>
    LIST USED PYTHON PACKAGES
    	modified:   initgit.sh
    	new file:   pip-packages.md

--- initgit.sh
+++ initgit.sh
@@ -2,7 +2,7 @@
 export GPGKEY=80BA91D1
 git config user.email "Billi@users.noreply.github.com"
 git config user.name "Billi"
-git config --global user.signingkey 80BA91D1
+git config --global user.signingkey $GPGKEY
 git config --global commit.gpgsign true
 # git remote set-url --push origin 'git@github.com:$myuser/$myrepo.git'
 
---
+++ pip-packages.md
@@ -0,0 +1,6 @@
+`#sudo pip freeze -l`
+Django==1.9.7
+MySQL-python==1.2.5
+django-mysql==1.0.9
+lpthw.web==1.1
+
